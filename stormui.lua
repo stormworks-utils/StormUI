b831d6525cb7712d3f5cde8e44ba73fa7c2a7349
maythe15 <123639761+maythe15@users.noreply.github.com>
2023-09-11 22:55:49 +0200

    Create stormui.lua

---
+++ stormui.lua
@@ -0,0 +1,166 @@
+StormUI={}
+function StormUI.Create(defaultPageBackground)
+    --defaultPage={widgets={},background=defaultPageBackground or {type="blank"}}
+    
+    local newUI={pages={}, currentPage='default', cticks=0}
+    function newUI.addPage(id, background)
+        --[[
+            background types:
+            0: blank, dont draw anything
+            1: fill; needs argument 'color'
+            2: map; needs argument pos={x,y,zoom}
+            3: custom; needs an argument 'func' as a callback that takes the page id as an argument
+        ]]--
+        local background=background or {type=0}
+        local page={widgets={},background=background, id=id}
+        function page.proccess(cursor, cticks)
+            for k,v in pairs(page.widgets) do
+                v.proccess(cursor, cticks)
+            end
+        end
+        function page.draw()
+            if page.background.type==1 then
+                StormUI.utils.setColorToList(page.background.color)
+                screen.drawRectF(0,0,screen.getWidth()+1, screen.getHeight()+1)
+            elseif page.background.type==2 then
+                screen.drawMap(page.background.pos[1], page.background.pos[2], page.background.pos[3])
+            elseif page.background.type==3 then
+                page.background.func(page.id)
+            end
+            for k,v in pairs(page.widgets) do
+                v.draw()
+            end
+        end
+        function page.addToggle(id, x, y, width, height, outline_color_on, outline_color_off, fill_color_on, fill_color_off, label_off, label_on, label_color_on, label_color_off, default)
+            local toggle={x=x,y=y,width=width, height=height, outline_color_on=outline_color_on, outline_color_off=outline_color_off, fill_color_on=fill_color_on, fill_color_off=fill_color_off, label_off=label_off, label_on=label_on, label_color_off=label_color_off, label_color_on=label_color_on, active=default, p_click=false, hold_length=0}
+            function toggle.proccess(cursor, cticks)
+                cursorx=cursor[1]
+                cursory=cursor[2]
+                x1=toggle.x
+                x2=toggle.width+x1
+                y1=toggle.y
+                y2=toggle.height+y1
+                pclick=toggle.p_click
+                inBounds=(cursorx>x1 and cursorx<x2 and cursory>y1 and cursory<y2)
+                toggle.p_click=inBounds and cursor[3]
+                if cursor[3] then
+                    if cticks==0 then
+                        toggled=toggle.active
+                        if inBounds and not pclick then
+                            active=not active
+                            toggle.active=active
+                        end
+                    end
+                    toggle.hold_length=toggle.hold_length+1
+                else
+                    toggle.hold_length=0
+                end
+            end
+            function toggle.draw()
+                if toggle.active then
+                    StormUI.utils.setColorToList(toggle.fill_color_on)
+                    screen.drawRectF(toggle.x, toggle.y, toggle.width, toggle.height)
+                    StormUI.utils.setColorToList(toggle.outline_color_on)
+                    screen.drawRect(toggle.x, toggle.y, toggle.width, toggle.height)
+                    StormUI.utils.setColorToList(toggle.label_color_on)
+                    screen.drawTextBox(toggle.x+1, toggle.y+1, toggle.width, toggle.height, toggle.label_on, 0, 0)
+                else
+                    StormUI.utils.setColorToList(toggle.fill_color_off)
+                    screen.drawRectF(toggle.x, toggle.y, toggle.width, toggle.height)
+                    StormUI.utils.setColorToList(toggle.outline_color_off)
+                    screen.drawRect(toggle.x, toggle.y, toggle.width, toggle.height)
+                    StormUI.utils.setColorToList(toggle.label_color_off)
+                    screen.drawTextBox(toggle.x+1, toggle.y+1, toggle.width, toggle.height, toggle.label_off, 0, 0)
+                end
+            end
+            page.widgets[id]=toggle
+        end
+        function page.addPush(id, x, y, width, height, outline_color_on, outline_color_off, fill_color_on, fill_color_off, label_off, label_on, label_color_on, label_color_off)
+            local push={x=x,y=y,width=width, height=height, outline_color_on=outline_color_on, outline_color_off=outline_color_off, fill_color_on=fill_color_on, fill_color_off=fill_color_off, label_off=label_off, label_on=label_on, label_color_off=label_color_off, label_color_on=label_color_on, hold_length=0}
+            function push.proccess(cursor, cticks)
+                cursorx=cursor[1]
+                cursory=cursor[2]
+                x1=push.x
+                x2=push.width+x1
+                y1=push.y
+                y2=push.height+y1
+                inBounds=(cursorx>x1 and cursorx<x2 and cursory>y1 and cursory<y2)
+                if cursor[3] then
+                    push.hold_length=push.hold_length+1
+                else
+                    push.hold_length=0
+                end
+                push.active=inBounds and cursor[3]
+            end
+            function push.draw()
+                if push.active then
+                    StormUI.utils.setColorToList(push.fill_color_on)
+                    screen.drawRectF(push.x, push.y, push.width, push.height)
+                    StormUI.utils.setColorToList(push.outline_color_on)
+                    screen.drawRect(push.x, push.y, push.width, push.height)
+                    StormUI.utils.setColorToList(push.label_color_on)
+                    screen.drawTextBox(push.x+1, push.y+1, push.width, push.height, push.label_on, 0, 0)
+                else
+                    StormUI.utils.setColorToList(push.fill_color_off)
+                    screen.drawRectF(push.x, push.y, push.width, push.height)
+                    StormUI.utils.setColorToList(push.outline_color_off)
+                    screen.drawRect(push.x, push.y, push.width, push.height)
+                    StormUI.utils.setColorToList(push.label_color_off)
+                    screen.drawTextBox(push.x+1, push.y+1, push.width, push.height, push.label_off, 0, 0)
+                end
+            end
+            page.widgets[id]=push
+        end
+        function page.addTextbox(id, x, y, width, height, outline_color, fill_color, label, label_color, center_x, center_y)
+            local textbox={x=x,y=y,width=width, height=height, outline_color=outline_color, fill_color=fill_color, label=label, label_color=label_color, center_x=center_x, center_y=center_y}
+            function textbox.proccess(cursor, cticks)
+            end
+            function textbox.draw()
+                StormUI.utils.setColorToList(textbox.fill_color)
+                screen.drawRectF(textbox.x, textbox.y, textbox.width, textbox.height)
+                StormUI.utils.setColorToList(textbox.outline_color)
+                screen.drawRect(textbox.x, textbox.y, textbox.width, textbox.height)
+                StormUI.utils.setColorToList(textbox.label_color)
+                screen.drawTextBox(textbox.x+1, textbox.y+1, textbox.width, textbox.height, textbox.label, textbox.center_x, textbox.center_y)
+            end
+            page.widgets[id]=textbox
+        end
+        newUI.pages[id]=page
+    end
+    function newUI.setPage(id)
+        if newUI.pages[id] then
+            newUI.currentPage=id
+        end
+    end
+    function newUI.process(cursor)
+        newUI.pages[newUI.currentPage].proccess(cursor, newUI.cticks)
+        if cursor[3] then
+            newUI.cticks=newUI.cticks+1
+        else
+            newUI.cticks=0
+        end
+    end
+    function newUI.draw()
+        newUI.pages[newUI.currentPage].draw()
+    end
+    function newUI.getPage()
+        return newUI.pages[newUI.currentPage]
+    end
+    newUI.addPage('default', defaultPageBackground or {type="blank"})
+    return newUI
+end
+StormUI.utils={}
+
+function StormUI.utils.setColorToList(list)
+    r=list[1]
+    g=list[2]
+    b=list[3]
+    a=list[4]
+    screen.setColor(r,g,b,a)
+end
+
+StormUI.colors={}
+
+StormUI.colors.Transparent={0,0,0,0}
+StormUI.colors.White={255,255,255,255}
+StormUI.colors.Black={0,0,0,255}
