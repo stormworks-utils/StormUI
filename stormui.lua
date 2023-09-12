30e1de3c89c41fbe910107e3e162b5b99f9733ab
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-09-12 17:50:20 +0200

    made StormUI easier to minify and added themes

--- stormui.lua
+++ stormui.lua
@@ -1,8 +1,41 @@
+-- for minifier to signal minification
+sc = screen
+
 StormUI={}
+--[[
+    ---Variables---
+    t: current ticks
+]]
 function StormUI.Create(defaultPageBackground)
     --defaultPage={widgets={},background=defaultPageBackground or {type="blank"}}
-    
-    local newUI={pages={}, currentPage='default', cticks=0}
+    local function isInRect(rect, cursor)
+        local x, y = cursor[1], cursor[2]
+        return cursor[3] and x >= rect.x and x < rect.x + rect.w and y >= rect.y and y < rect.y + rect.w
+    end
+
+    local function drawRectF(rect)
+        sc.drawRectF(rect.x, rect.y, rect.w, rect.h)
+    end
+
+    local function drawRect(rect)
+        sc.drawRect(rect.x, rect.y, rect.w, rect.h)
+    end
+
+    local function drawTextBox(rect, text, center_x, center_y)
+        sc.drawTextBox(rect.x+1, rect.y+1, rect.w, rect.h, text, center_x or 0, center_y or 0)
+    end
+
+    local newUI={pages={}, currentPage='default', t=0, theme={}}
+
+    local function get_option(custom_options, name, subtype, default)
+        name = name .. (subtype and "_" .. subtype or "")
+        return (custom_options and custom_options[name]) or newUI.theme[name] or default
+    end
+
+    local function apply_color(custom_options, name, subtype)
+        StormUI.utils.setColorToList(get_option(custom_options, name, subtype, {255, 0, 255}))
+    end
+
     function newUI.addPage(id, background)
         --[[
             background types:
@@ -11,41 +44,46 @@ function StormUI.Create(defaultPageBackground)
             2: map; needs argument pos={x,y,zoom}
             3: custom; needs an argument 'func' as a callback that takes the page id as an argument
         ]]--
-        local background=background or {type=0}
-        local page={widgets={},background=background, id=id}
-        function page.proccess(cursor, cticks)
+        local page={widgets={},background=background or {type=0}, id=id}
+        function page.proccess(cursor, t)
             for k,v in pairs(page.widgets) do
-                v.proccess(cursor, cticks)
+                v.proccess(cursor, t)
             end
         end
         function page.draw()
-            if page.background.type==1 then
-                StormUI.utils.setColorToList(page.background.color)
-                screen.drawRectF(0,0,screen.getWidth()+1, screen.getHeight()+1)
-            elseif page.background.type==2 then
-                screen.drawMap(page.background.pos[1], page.background.pos[2], page.background.pos[3])
-            elseif page.background.type==3 then
-                page.background.func(page.id)
+            local backgroud = page.background
+            if background.type==1 then
+                StormUI.utils.setColorToList(background.color)
+                sc.drawClear()
+            elseif background.type==2 then
+                sc.drawMap(background.pos[1], background.pos[2], background.pos[3])
+            elseif background.type==3 then
+                background.func(page.id)
             end
             for k,v in pairs(page.widgets) do
                 v.draw()
             end
         end
-        function page.addToggle(id, x, y, width, height, outline_color_on, outline_color_off, fill_color_on, fill_color_off, label_off, label_on, label_color_on, label_color_off, default)
-            local toggle={x=x,y=y,width=width, height=height, outline_color_on=outline_color_on, outline_color_off=outline_color_off, fill_color_on=fill_color_on, fill_color_off=fill_color_off, label_off=label_off, label_on=label_on, label_color_off=label_color_off, label_color_on=label_color_on, active=default, p_click=false, hold_length=0}
-            function toggle.proccess(cursor, cticks)
-                cursorx=cursor[1]
-                cursory=cursor[2]
-                x1=toggle.x
-                x2=toggle.width+x1
-                y1=toggle.y
-                y2=toggle.height+y1
+
+        --[[
+            Supported custom options:
+             - outline_color_on
+             - outline_color_off
+             - fill_color_on
+             - fill_color_off
+             - label_color_on
+             - label_color_off
+             - label_on
+             - label_off
+        ]]
+        function page.addToggle(id, x, y, width, height, default, custom_options)
+            local toggle={x=x, y=y, w=width, h=height, o=custom_options, active=default, p_click=false, hold_length=0}
+            function toggle.proccess(cursor, t)
                 pclick=toggle.p_click
-                inBounds=(cursorx>x1 and cursorx<x2 and cursory>y1 and cursory<y2)
-                toggle.p_click=inBounds and cursor[3]
+                inBounds = isInRect(toggle, cursor)
+                toggle.p_click = inBounds
                 if cursor[3] then
-                    if cticks==0 then
-                        toggled=toggle.active
+                    if t==0 then
                         if inBounds and not pclick then
                             active=not active
                             toggle.active=active
@@ -61,125 +99,136 @@ function StormUI.Create(defaultPageBackground)
                 end
             end
             function toggle.draw()
-                if toggle.active then
-                    StormUI.utils.setColorToList(toggle.fill_color_on)
-                    screen.drawRectF(toggle.x, toggle.y, toggle.width, toggle.height)
-                    StormUI.utils.setColorToList(toggle.outline_color_on)
-                    screen.drawRect(toggle.x, toggle.y, toggle.width, toggle.height)
-                    StormUI.utils.setColorToList(toggle.label_color_on)
-                    screen.drawTextBox(toggle.x+1, toggle.y+1, toggle.width, toggle.height, toggle.label_on, 0, 0)
-                else
-                    StormUI.utils.setColorToList(toggle.fill_color_off)
-                    screen.drawRectF(toggle.x, toggle.y, toggle.width, toggle.height)
-                    StormUI.utils.setColorToList(toggle.outline_color_off)
-                    screen.drawRect(toggle.x, toggle.y, toggle.width, toggle.height)
-                    StormUI.utils.setColorToList(toggle.label_color_off)
-                    screen.drawTextBox(toggle.x+1, toggle.y+1, toggle.width, toggle.height, toggle.label_off, 0, 0)
-                end
+                local subtype = toggle.active and "on" or "off"
+                apply_color(toggle.o, "fill_color", subtype)
+                drawRectF(toggle)
+                apply_color(toggle.o, "outline_color", subtype)
+                drawRect(toggle)
+                apply_color(toggle.o, "label_color", subtype)
+                drawTextBox(toggle, get_option(toggle.o, "label", subtype, subtype))
             end
             page.widgets[id]=toggle
         end
-        function page.addPush(id, x, y, width, height, outline_color_on, outline_color_off, fill_color_on, fill_color_off, label_off, label_on, label_color_on, label_color_off)
-            local push={x=x,y=y,width=width, height=height, outline_color_on=outline_color_on, outline_color_off=outline_color_off, fill_color_on=fill_color_on, fill_color_off=fill_color_off, label_off=label_off, label_on=label_on, label_color_off=label_color_off, label_color_on=label_color_on, hold_length=0}
-            function push.proccess(cursor, cticks)
-                cursorx=cursor[1]
-                cursory=cursor[2]
-                x1=push.x
-                x2=push.width+x1
-                y1=push.y
-                y2=push.height+y1
-                inBounds=(cursorx>x1 and cursorx<x2 and cursory>y1 and cursory<y2)
-                if cursor[3] and inBounds then
-                    push.hold_length=push.hold_length+1
+
+        --[[
+            Supported custom options:
+             - outline_color_on
+             - outline_color_off
+             - fill_color_on
+             - fill_color_off
+             - label_color_on
+             - label_color_off
+             - label_on
+             - label_off
+        ]]
+        function page.addPush(id, x, y, width, height, custom_options)
+            local push={x=x, y=y, w=width, h=height, o=custom_options, hold_length=0}
+            function push.proccess(cursor, t)
+                inBounds = isInRect(push, cursor)
+                if inBounds then
+                    push.hold_length = push.hold_length+1
                 else
-                    push.hold_length=0
+                    push.hold_length = 0
                 end
-                push.active=inBounds and cursor[3]
+                push.active = inBounds
             end
             function push.draw()
-                if push.active then
-                    StormUI.utils.setColorToList(push.fill_color_on)
-                    screen.drawRectF(push.x, push.y, push.width, push.height)
-                    StormUI.utils.setColorToList(push.outline_color_on)
-                    screen.drawRect(push.x, push.y, push.width, push.height)
-                    StormUI.utils.setColorToList(push.label_color_on)
-                    screen.drawTextBox(push.x+1, push.y+1, push.width, push.height, push.label_on, 0, 0)
-                else
-                    StormUI.utils.setColorToList(push.fill_color_off)
-                    screen.drawRectF(push.x, push.y, push.width, push.height)
-                    StormUI.utils.setColorToList(push.outline_color_off)
-                    screen.drawRect(push.x, push.y, push.width, push.height)
-                    StormUI.utils.setColorToList(push.label_color_off)
-                    screen.drawTextBox(push.x+1, push.y+1, push.width, push.height, push.label_off, 0, 0)
-                end
+                local subtype = push.active and "on" or "off"
+                apply_color(push.o, "fill_color", subtype)
+                drawRectF(push)
+                apply_color(push.o, "outline_color", subtype)
+                drawRect(push)
+                apply_color(push.o, "label_color", subtype)
+                drawTextBox(push, get_option(push.o, "label", subtype, subtype))
             end
             page.widgets[id]=push
         end
-        function page.addTextbox(id, x, y, width, height, outline_color, fill_color, label, label_color, center_x, center_y)
-            local textbox={x=x,y=y,width=width, height=height, outline_color=outline_color, fill_color=fill_color, label=label, label_color=label_color, center_x=center_x, center_y=center_y}
-            function textbox.proccess(cursor, cticks)
+
+        --[[
+            Supported custom options:
+             - outline_color
+             - fill_color
+             - label_color
+             - center_x
+             - center_y
+        ]]
+        function page.addTextbox(id, x, y, width, height, label, custom_options)
+            local textbox={x=x, y=y, w=width, h=height, label=label, o=custom_options}
+            function textbox.proccess(cursor, t)
             end
             function textbox.draw()
-                StormUI.utils.setColorToList(textbox.fill_color)
-                screen.drawRectF(textbox.x, textbox.y, textbox.width, textbox.height)
-                StormUI.utils.setColorToList(textbox.outline_color)
-                screen.drawRect(textbox.x, textbox.y, textbox.width, textbox.height)
-                StormUI.utils.setColorToList(textbox.label_color)
-                screen.drawTextBox(textbox.x+1, textbox.y+1, textbox.width, textbox.height, textbox.label, textbox.center_x, textbox.center_y)
+                apply_color(textbox.o, "fill_color")
+                drawRectF(textbox)
+                apply_color(textbox.o, "outline_color")
+                drawRect(textbox)
+                apply_color(textbox.o, "label_color")
+                drawTextBox(textbox, textbox.label)
             end
             page.widgets[id]=textbox
         end
         function page.addLine(id, x1, y1, x2, y2, color)
             local line={x1=x1,y1=y1,x2=x2, y2=y2, color=color}
-            function line.proccess(cursor, cticks)
+            function line.proccess(cursor, t)
             end
             function line.draw()
                 StormUI.utils.setColorToList(line.color)
-                screen.drawLine(line.x1, line.y1, line.x2, line.y2)
+                sc.drawLine(line.x1, line.y1, line.x2, line.y2)
             end
             page.widgets[id]=line
         end
-        function page.addRect(id, x, y, width, height, border_color, fill_color)
-            local rect={x=x,y=y, width=width, height=height, border_color=border_color, fill_color=fill_color}
-            function rect.proccess(cursor, cticks)
+
+        --[[
+            Supported custom options:
+             - outline_color
+             - fill_color
+        ]]
+        function page.addRect(id, x, y, width, height, custom_options)
+            local rect={x=x, y=y, w=width, h=height, o=custom_options}
+            function rect.proccess(cursor, t)
             end
             function rect.draw()
-                StormUI.utils.setColorToList(rect.fill_color)
-                screen.drawRectF(rect.x, rect.y, rect.width, rect.height)
-                StormUI.utils.setColorToList(rect.border_color)
-                screen.drawRect(rect.x, rect.y, rect.width, rect.height)
+                apply_color(rect.o, "fill_color")
+                drawRectF(rect)
+                apply_color(rect.o, "outline_color")
+                drawRect(rect)
             end
             page.widgets[id]=rect
         end
-        function page.addIndicatorRect(id, x, y, width, height, border_color, fill_color_on, fill_color_off)
-            local rectIndic={x=x,y=y, width=width, height=height, border_color=border_color, fill_color_on=fill_color_on, fill_color_off=fill_color_off, active=false}
-            function rectIndic.proccess(cursor, cticks)
+
+        --[[
+            Supported custom options:
+             - outline_color
+             - fill_color_on
+             - fill_color_off
+        ]]
+        function page.addIndicatorRect(id, x, y, width, height, custom_options)
+            local rectIndic={x=x, y=y, w=width, h=height, o=custom_options, active=false}
+            function rectIndic.proccess(cursor, t)
             end
             function rectIndic.draw()
-                if rectIndic.active then
-                    StormUI.utils.setColorToList(rectIndic.fill_color_on)
-                else
-                    StormUI.utils.setColorToList(rectIndic.fill_color_off)
-                end
-                screen.drawRectF(rectIndic.x, rectIndic.y, rectIndic.width, rectIndic.height)
-                StormUI.utils.setColorToList(rectIndic.border_color)
-                screen.drawRect(rectIndic.x, rectIndic.y, rectIndic.width, rectIndic.height)
+                apply_color(rectIndic.o, "fill_color", rectIndic.active and "on" or "off")
+                drawRectF(rectIndic)
+                apply_color(rectIndic.o, "outline_color")
+                drawRect(rectIndic)
             end
             page.widgets[id]=rectIndic
         end
-        function page.addIndicatorCircle(id, x, y, radius, border_color, fill_color_on, fill_color_off)
-            local circIndic={x=x,y=y, radius=radius, border_color=border_color, fill_color_on=fill_color_on, fill_color_off=fill_color_off, active=false}
-            function circIndic.proccess(cursor, cticks)
+
+        --[[
+            Supported custom options:
+             - outline_color
+             - fill_color_on
+             - fill_color_off
+        ]]
+        function page.addIndicatorCircle(id, x, y, radius, custom_options)
+            local circIndic={x=x,y=y, r=radius, o=custom_options, active=false}
+            function circIndic.proccess(cursor, t)
             end
             function circIndic.draw()
-                if circIndic.active then
-                    StormUI.utils.setColorToList(circIndic.fill_color_on)
-                else
-                    StormUI.utils.setColorToList(circIndic.fill_color_off)
-                end
-                screen.drawCircleF(circIndic.x, circIndic.y, circIndic.radius)
-                StormUI.utils.setColorToList(circIndic.border_color)
-                screen.drawCircle(circIndic.x, circIndic.y, circIndic.radius)
+                apply_color(circIndic.o, "fill_color", rectIndic.active and "on" or "off")
+                sc.drawCircleF(circIndic.x, circIndic.y, circIndic.r)
+                apply_color(circIndic.o, "outline_color")
+                sc.drawCircle(circIndic.x, circIndic.y, circIndic.r)
             end
             page.widgets[id]=circIndic
         end
@@ -191,11 +240,11 @@ function StormUI.Create(defaultPageBackground)
         end
     end
     function newUI.process(cursor)
-        newUI.pages[newUI.currentPage].proccess(cursor, newUI.cticks)
+        newUI.pages[newUI.currentPage].proccess(cursor, newUI.t)
         if cursor[3] then
-            newUI.cticks=newUI.cticks+1
+            newUI.t=newUI.t+1
         else
-            newUI.cticks=0
+            newUI.t=0
         end
     end
     function newUI.draw()
@@ -210,11 +259,7 @@ end
 StormUI.utils={}
 
 function StormUI.utils.setColorToList(list)
-    r=list[1]
-    g=list[2]
-    b=list[3]
-    a=list[4]
-    screen.setColor(r,g,b,a)
+    sc.setColor(list[1],list[2],list[3],list[4])
 end
 
 StormUI.colors={}
